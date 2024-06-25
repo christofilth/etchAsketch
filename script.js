86b9499a419caa1b647b7da6cb4180fa9191a02f
christofilth <171975741+christofilth@users.noreply.github.com>
2024-06-25 17:01:30 +0100

    Add createGrid function to script
    Added a new function called createGrid, which asks for 2 arguments,
    column and row. The idea is that when the for loop is active, it adds a
    new cell until result of column * row is reached.

---
+++ script.js
@@ -0,0 +1,10 @@
+function createGrid(column,row){
+    const container = document.getElementById("container");
+    for (i = 0; i < column * row; i++){
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        container.appendChild(cell);
+    }
+}
+
+createGrid(16,16);
